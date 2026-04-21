53a1fe78b215a023b7be9c5d5395a5bc0ec91175
Vadim Uvarov <uvarov.vadim42@gmail.com>
2026-04-21 16:19:18 +0200

    build(terraform): rename resource prefix and increase lambda timeout

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,11 +7,11 @@ variable "region" {
 variable "project" {
   description = "Name prefix applied to every resource."
   type        = string
-  default     = "task-queue-demo"
+  default     = "aws-demo-simple-task-queue"
 }
 
 variable "max_seconds" {
   description = "Upper bound on the user-submitted sleep duration."
   type        = number
-  default     = 30
+  default     = 5 * 60  # 5 minutes
 }
