57cd54b65bba68fc1f5aa4cfd361c1409f3cb447
Vadim Uvarov <uvarov.vadim42@gmail.com>
2026-05-08 20:29:42 +0200

    feat: change max_seconds (to cap user input) back to 30

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,7 +13,7 @@ variable "project" {
 variable "max_seconds" {
   description = "Upper bound on the user-submitted sleep duration."
   type        = number
-  default     = 5 * 60 # 5 minutes
+  default     = 30
 }
 
 variable "max_queue_size" {
